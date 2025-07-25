20511aa3088fb8771a36186c98d11aa16a16e3fb
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-25 20:22:27 +0530

    pokemon repository test

---
+++ src/test/java/com/pokemonreview/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiApplicationTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
